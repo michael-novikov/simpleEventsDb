ee8150eee5862047e05f0657b1da17232b71c9f2
michael-novikov <michael.a.novikov@yandex.com>
2019-01-15 19:37:54 +0300

    feat: add operator<

--- database.cpp
+++ database.cpp
@@ -71,6 +71,23 @@ Date Date::ParseDate(const string& s) {
 	return Date(year, month, day);
 }
 
+bool operator<(const Date& lhs, const Date& rhs) {
+	if (lhs.GetYear() < rhs.GetYear()) {
+		return true;
+	}
+	else if ((lhs.GetYear() == rhs.GetYear())
+			&& (lhs.GetMonth() < rhs.GetMonth())) {
+		return true;
+	}
+	else if ((lhs.GetYear() == rhs.GetYear())
+			&& (lhs.GetMonth() == rhs.GetMonth())
+			&& (lhs.GetDay() < rhs.GetDay())) {
+		return true;
+	}
+
+	return false;
+}
+
 bool operator==(const Date& lhs, const Date& rhs) {
 	return (lhs.GetYear() == rhs.GetYear())
 			&& (lhs.GetMonth() == rhs.GetMonth())
--- test.cpp
+++ test.cpp
@@ -25,6 +25,26 @@ BOOST_AUTO_TEST_CASE(DateGetters) {
 	BOOST_REQUIRE_EQUAL(d.GetDay(), 1);
 }
 
+BOOST_AUTO_TEST_CASE(DateOperators) {
+	const Date d1(2000, 1, 1);
+	const Date d2(2000, 1, 1);
+	BOOST_CHECK(d1 == d2);
+
+	const Date d3(2001, 1, 1);
+	BOOST_CHECK(!(d1 == d3));
+	BOOST_CHECK(d1 < d3);
+
+	const Date d4(2000, 2, 1);
+	BOOST_CHECK(d1 < d4);
+
+	const Date d5(2000, 1, 2);
+	BOOST_CHECK(d1 < d5);
+
+	const Date d6(2002, 1, 2);
+	const Date d7(2001, 2, 1);
+	BOOST_CHECK(!(d6 < d7));
+}
+
 BOOST_AUTO_TEST_CASE(DateToString) {
 	const Date d1(2019, 12, 31);
 	BOOST_REQUIRE_EQUAL(d1.ToString(), "2019-12-31");
