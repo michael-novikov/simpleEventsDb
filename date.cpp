d40d4ffdd4ede2c1712b533279990de186d1cfb1
michael-novikov <michael.a.novikov@yandex.com>
2019-04-28 16:26:24 +0300

    add date implementation

---
+++ date.cpp
@@ -0,0 +1,116 @@
+#include "date.h"
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+Date::Date(int year, int month, int day)
+{
+	if (month < 1 || month > 12) {
+		throw domain_error("Month value is invalid: " + to_string(month));
+	}
+
+	if (day < 1 || day > 31) {
+		throw domain_error("Day value is invalid: " + to_string(day));
+	}
+
+	this->year = year;
+	this->month = month;
+	this->day = day;
+}
+
+int Date::GetYear() const
+{
+	return year;
+}
+
+int Date::GetMonth() const
+{
+	return month;
+}
+
+int Date::GetDay() const
+{
+	return day;
+}
+
+string Date::ToString() const
+{
+	stringstream stream;
+
+	stream << setw(4) << setfill('0') << GetYear() << '-'
+			<< setw(2) << setfill('0') << GetMonth() << '-'
+			<< setw(2) << setfill('0') << GetDay();
+
+	return stream.str();
+}
+
+bool operator<(const Date& lhs, const Date& rhs)
+{
+	if (lhs.GetYear() < rhs.GetYear()) {
+		return true;
+	}
+	else if ((lhs.GetYear() == rhs.GetYear())
+			&& (lhs.GetMonth() < rhs.GetMonth())) {
+		return true;
+	}
+	else if ((lhs.GetYear() == rhs.GetYear())
+			&& (lhs.GetMonth() == rhs.GetMonth())
+			&& (lhs.GetDay() < rhs.GetDay())) {
+		return true;
+	}
+
+	return false;
+}
+
+bool operator==(const Date& lhs, const Date& rhs)
+{
+	return (lhs.GetYear() == rhs.GetYear())
+			&& (lhs.GetMonth() == rhs.GetMonth())
+			&& (lhs.GetDay() == rhs.GetDay());
+}
+
+ostream& operator<<(ostream& out, const Date& d)
+{
+	return out << d.ToString();
+}
+
+istream& operator>>(istream& in, Date& d)
+{
+	d = ParseDate(in);
+	return in;
+}
+
+Date ParseDate(istream& stream) {
+	string error = "Wrong date format.";
+	int year, month, day;
+
+	if (!(stream >> year)) {
+		throw invalid_argument(error);;
+	}
+
+	if (stream.peek() != '-') {
+		throw invalid_argument(error);
+	}
+
+	stream.ignore(1);
+	stream >> month;
+
+	if (stream.peek() != '-') {
+		throw invalid_argument(error);
+	}
+
+	stream.ignore(1);
+	stream >> day;
+
+	if (stream.rdbuf()->in_avail()) {
+		throw invalid_argument(error);
+	}
+
+	return Date(year, month, day);
+}
+
+Date ParseDate(const string& s) {
+	istringstream is { s };
+	return ParseDate(is);
+}
---
+++ date.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Date {
+public:
+	Date(int year = 0, int month = 1, int day = 1);
+
+	int GetYear() const;
+	int GetMonth() const;
+	int GetDay() const;
+
+	string ToString() const;
+private:
+	int year;
+	int month;
+	int day;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+bool operator==(const Date& lhs, const Date& rhs);
+ostream& operator<<(ostream& out, const Date& d);
+istream& operator>>(istream& out, Date& d);
+
+Date ParseDate(istream& stream);
+Date ParseDate(const string& s);
